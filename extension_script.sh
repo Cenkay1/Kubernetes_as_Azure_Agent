a9e1195ed1c5e0f403ae0e5032fe52f362cf72dc
CenkayYaman <92253763+Cenkay1@users.noreply.github.com>
2024-09-10 15:28:32 +0300

    Create extension_script.sh

---
+++ extension_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+echo "------ Installing Kubectl -------"
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/bin/kubectl
